3d2439b6fce9609880032762688ba1f86653a816
01Eddie <jesus.11.97@hotmail.com>
2021-06-24 20:18:58 -0500

    [ADD] base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,38 @@
-#!/usr/bin/python3^M
-
+#!/usr/bin/python3
+"""Represents the base model to all classes for this project."""
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
-   
-	def __init__(self):
-		"""make a random UUID in this case uuid4: Generate a random UUID"""
-		self.id = str(uuid4())
-		"""created_at: datetime - assign with the current datetime when an instance is created"""
-		self.created_at = datetime.today()
-		"""updated_at: datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object"""
-		self.updated_at = datetime.today()
-	
-	""" __str__: should print: [<class name>] (<self.id>) <self.__dict__>"""
-	def __str__(self):
-		return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+    """Defines a Base Model class to main methods."""
 
-	def save(self):
-		"""save(self): updates the public instance attribute updated_at with the current datetime"""
-		return self.updated_at
+    def __init__(self):
+        """Initialize public attributes
 
-	def to_dict(self):
-		""" returns a dictionary containing all keys/values of __dict__ of the instance:"""
-		
-		dic = self.__dict__
-		dic['__class__'] = cls.__name__
-		dic['created_at'] = self.created_at.isoformat()
-		dic['updated_at'] = self.updated_at.isoformat()
-		return dic
-		"""
-		* by using self.__dict__, only instance attributes set will be returned 
-		* a key __class__ must be added to this dictionary with the class name of the object
-		* created_at and updated_at must be converted to string object in ISO format
-		** format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
-		** you can use isoformat() of datetime object
-		* This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel
-		"""
-		
+        Args:
+        updated_at (time): assign current datetime when instance is created.
+                       and will be updated every time you change your object.
+        id (str): Generate a random UUID
+        created_at (time): Assign current datetime when instance is created.
+        """
+
+        self.updated_at = datetime.today()
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+
+    def __str__(self):
+        """Return the human readable string format."""
+        return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+
+    def save(self):
+        """Updates the public instance attribute with the current datetime"""
+        return self.updated_at
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance."""
+        dic = self.__dict__
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
