271a26a407f05dea47c22e7c0a0661fe26447793
luisdev20 <lescobedo.im@gmail.com>
2021-06-26 22:29:13 -0500

    [ADD] create method

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Console that contains the entry point of the command interpreter."""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,6 +12,10 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User"
+    }
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -24,6 +29,20 @@ class HBNBCommand(cmd.Cmd):
         """Function empty line"""
         pass
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel and save to JSON file"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] in HBNBCommand.__classes:
+            new_ins = eval(arg[0])()
+            new_ins.save()
+            print(new_ins.id)
+        else:
+            print("** class doesn't exist **")
+
+def parse(arg):
+    "tokenize a string to a space divided 'arguments' tuple"
+    return tuple(map(int, arg.split()))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
